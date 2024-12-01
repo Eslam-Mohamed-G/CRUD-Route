e491a929587d2d68a13e45e2b44b405ae432a077
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-12-01 18:08:08 +0200

    add vriabls to js

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var nameInput = document.getElementById("nameInput");
+var priceInput = document.getElementById("priceInput");
+var category = document.getElementById("category");
+var countInput = document.getElementById("count");
+var descripInput =document.getElementById("descripInput");
+var submitBTN = document.getElementById("btn");
+var searchInput = document.getElementById("searchInput");
+var filter = document.getElementById("filter");
