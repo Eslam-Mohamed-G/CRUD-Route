91d4e09cb3cf5da4de7e7ebd63e0c7cfb400bdbe
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-12-01 19:39:15 +0200

    add function validateprice to check current name

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ var table = document.getElementById("productTable").querySelector("tbody");
 function validateName() {
     var messageError = document.getElementById("nameError");
     var name = nameInput.value;
-    var nameRegex = /^[a-zA-Z]{3,}$/;   //The name starts with at least three letters
+    var nameRegex = /^[a-zA-Z]{3,}.*$/;   //The name starts with at least three letters
 
     if(!name){
         messageError.textContent = "Name required";
@@ -31,4 +31,27 @@ function validateName() {
     }
 
 }
-nameInput.addEventListener("input",validateName)
+nameInput.addEventListener("input",validateName);
+
+function validatePrice() {
+    var priceError = document.getElementById("priceError");
+    var price = priceInput.value;
+    var priceRegex = /^\d+(\.\d+)?$/;
+
+    if(!price){
+        priceError.textContent = "price required";
+        priceInput.style.border = "1px solid red";
+        return false;
+    }else {
+        if(!priceRegex.test(price)){
+            priceInput.style.border = "1px solid red";
+            priceError.textContent = "must be valid";
+            return false;
+        }else{
+            priceError.textContent = "";
+            priceInput.style.border = "1px solid #ddd"
+            return true;
+        }
+    }
+}
+priceInput.addEventListener("input", validatePrice);
