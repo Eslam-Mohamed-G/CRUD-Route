2035583caf98fe769d5b86e63d3582e346b20caa
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-12-01 19:06:42 +0200

    add function validatename te check current name

--- assets/script.js
+++ assets/script.js
@@ -7,4 +7,28 @@ var submitBTN = document.getElementById("btn");
 var searchInput = document.getElementById("searchInput");
 var filter = document.getElementById("filter");
 
-var table = document.getElementById("productTable").querySelector("tbody")
+var table = document.getElementById("productTable").querySelector("tbody");
+
+function validateName() {
+    var messageError = document.getElementById("nameError");
+    var name = nameInput.value;
+    var nameRegex = /^[a-zA-Z]{3,}$/;   //The name starts with at least three letters
+
+    if(!name){
+        messageError.textContent = "Name required";
+        nameInput.style.border = "1px solid red";
+        return false;
+    }else {
+        if(!nameRegex.test(name)){
+            messageError.textContent = "must be valid";
+            nameInput.style.border = "1px solid red";
+            return false;
+        }else{
+            messageError.textContent = "";
+            nameInput.style.border = "1px solid #ddd"
+            return true;
+        }
+    }
+
+}
+nameInput.addEventListener("input",validateName)
