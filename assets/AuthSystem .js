f349aee6ca2ec314179308f87c988061fc690ecb
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-12-18 11:57:46 +0200

    add logIn method

--- assets/AuthSystem .js
+++ assets/AuthSystem .js
@@ -1,5 +1,11 @@
+import StorageData from "./storageData.js";
 class AuthSystem{
-    
+
+    static logIn(name, password) {
+        const userArray = StorageData.userArray;
+        const user = userArray.find(user => user.name === name && user.password === password);
+        return user || null
+    }
 }
 
 export default  AuthSystem;
