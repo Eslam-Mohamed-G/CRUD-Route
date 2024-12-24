03d2536ee3fb9dc5f3d07526a7f45f7905db5f28
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-12-24 14:08:15 +0200

    add constructor to Products class

--- assets/Products.js
+++ assets/Products.js
@@ -1,7 +1,14 @@
 import StorageManager from "./StorageManager.js";
 import IsValid from "./isValid.js";
 class Products {
+    constructor(){
+        this.productContainer = document.getElementById("productTable");
+        this.validator = new IsValid();
+    }
 
+    addproduct(name, id, price, category, count) {
+
+    }
 }
 
 export default Products;
