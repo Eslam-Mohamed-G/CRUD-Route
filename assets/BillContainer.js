4dcc49097a803fa8cf98690f148002fcabe25c71
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-01 12:42:57 +0200

    add isFormBillValid() and showeBillByUserName()

--- assets/BillContainer.js
+++ assets/BillContainer.js
@@ -2,7 +2,7 @@ import StorageManager from "./StorageManager.js";
 import IsValid from "./isValid.js";
 
 class BillContainer {
-    constructor(){
+    constructor() {
         this.nameInput = document.getElementById("BillUserNameInput");
         this.dateInput = document.getElementById("billDateInput");
 
@@ -11,6 +11,42 @@ class BillContainer {
 
         this.billArray = StorageManager.loadBillData();
         this.validator = new IsValid();
+    };
+
+    isFormBillValid() {
+        const isNameInputValid = this.validator.isValidName(nameInput.value);
+        const isDateInputValid = this.dateInput.value ? this.dateInput.value : false;
+
+        this.validator.setInputState(nameInput, isNameInputValid);
+        this.validator.setInputState(dateInput, isDateInputValid);
+
+        return (isNameInputValid && isDateInputValid);
+    };
+
+    showeBillByUserName() {
+        const isNameInputValid = this.validator.isValidName(nameInput.value);
+        this.validator.setInputState(nameInput, isNameInputValid);
+
+        this.billArray.forEach((element, index) => {
+            const billColl = document.createElement("div");
+            billColl.classList.add("col");
+
+            billColl.innerHTML =
+            `
+                <div class="card text-bg-light mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Light card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+
+            `;
+        });
+
+        if (isNameInputValid) {
+
+        }
     };
 }
 export default BillContainer;
